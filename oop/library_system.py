8e169ce3cdf084e93511a9507cc8d0543173b51c
edwin kapule <greatkapule@gmail.com>
2025-12-07 06:11:56 +0300

    fix(oop): Renamed __str__ to get_details() and fixed output formatting for checker

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,26 +1,26 @@
 class Book:
-    def __init__(self, title: str, author:str):
+    def __init__(self, title: str, author: str):
         self.title = title
         self.author = author
 
-    def __str__(self):
-        return f"Book:{self.title} by {self.author}"
+    def get_details(self):
+        return f"Book: {self.title} by {self.author}"
 
 class EBook(Book):
     def __init__(self, title: str, author: str, file_size: int):
         super().__init__(title, author)
         self.file_size = file_size
 
-    def __str__(self):
-        return f"EBook: {self.title} by {self.author}, File size: {self.file_size}KB"
+    def get_details(self):
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
 
 class PrintBook(Book):
     def __init__(self, title: str, author: str, page_count: int):
         super().__init__(title, author)
         self.page_count = page_count
 
-    def __str__(self):
-        return f"PrintBook: {self.title} by {self.author}, Page count: {self.page_count}"
+    def get_details(self):
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
      
 class Library:
     def __init__(self):
@@ -31,4 +31,4 @@ class Library:
 
     def list_books(self):
         for book in self.books:
-            print(book)
+            print(book.get_details())
